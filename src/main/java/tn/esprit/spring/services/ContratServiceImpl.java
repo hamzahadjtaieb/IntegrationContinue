631560429a3f2c6979af1224c6d27cf26b4b58d4
Hamza Hadj Taieb <mohamedhamza.hadjtaieb@esprit.tn>
2020-10-20 22:42:06 +0200

    Hamza Commit

--- src/main/java/tn/esprit/spring/services/ContratServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/ContratServiceImpl.java
@@ -2,14 +2,18 @@ package tn.esprit.spring.services;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import tn.esprit.Log4j.AffichageLog;
 import tn.esprit.spring.entities.Contrat;
 import tn.esprit.spring.repository.ContratRepository;
 
 @Service
 public class ContratServiceImpl implements IContratService {
+	
+	private static final Logger l = Logger.getLogger(AffichageLog.class);
 
 
 	@Autowired
@@ -17,6 +21,7 @@ public class ContratServiceImpl implements IContratService {
 
 
 	public List<Contrat> getAllContrats() {
+	l.info("Getting the list of contrats");
 		return (List<Contrat>) contratRepository.findAll();
 	}
 
--- src/test/java/tn/esprit/spring/ContratServiceImplTest.java
+++ src/test/java/tn/esprit/spring/ContratServiceImplTest.java
@@ -11,29 +11,31 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.beans.factory.annotation.;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import tn.esprit.spring.entities.Departement;
-import tn.esprit.spring.services.IDepartementService;
+import tn.esprit.spring.entities.Contrat;
+import tn.esprit.spring.services.IContratService;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DepartementServiceImplTest {
+public class ContratServiceImplTest {
+
+
 	
 	
 	/* ******************** JUNIT TEST CLASS AKTHEM ARFAOUI ***************************  */
 
 	@Autowired 
-	IDepartementService ds; 
+	IContratService Cs; 
 
 
 
 	@Test
-	public void testRetrieveAllDepartment() {
-		List<Departement> listDepartment = ds.getAllDepartements(); 
-		// if there are 5 users in DB : 
-		assertEquals(5, listDepartment.size());
+	public void testRetrieveAllContrat() {
+		List<Contrat> listContrat = Cs.getAllContrats(); 
+		// if there are 7 users in DB : 
+		assertEquals(7, listContrat.size());
 	}
 
 	
